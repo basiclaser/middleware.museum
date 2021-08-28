756b0873567b06b6beb90a54f74ea08a94182c9d
z-memory <dont@forgetmy.email>
2021-08-28 12:41:40 +0200

    clean up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // TOP TEN MIDDLEWARES
 
-// sessions 
+// 0. sessions 
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -17,43 +17,46 @@ app.use(session({
     cookie: { secure: false }
   }))
 
-// 0. logging - allows us to log requests
+// 1. logging - allows us to log requests
 app.use((req, res, next) => {
     // console.log(`${req.method} ${req.path}`)
     console.log(req.sessionID)
     next()
 })
 
-// 1. morgan - allows us to log requests
-// app.use(morgan('dev'))
+// 2. morgan - allows us to log requests
+app.use(morgan('dev'))
 
-// 2. morgan - write logs to a file
+// 3. morgan - morgan can also logs to a file
 var accessLogStream = fs.createWriteStream(path.join(process.cwd(), 'access.log'), { flags: 'a' })
 app.use(morgan('combined', { stream: accessLogStream }))
 
-// 3. CORS - allows us to send requests from different domains
+// 4. CORS - allows us to send requests from different webpages 
+// on different domains. if our frontend is on 3000 and our backend is on 8080
+// we need to enable CORS so the frontend can send requests to our backend
 app.use(cors({
     origin: "http://localhost:3000" // or your netlify domain
 }))
 
-// 4. express.json - allows us to read the body of the request
+// 5. express.json - allows us to read the JSON body of a request
 app.use(express.json())
 
-// 5. express.urlencoded - allows us to read form data  
+// 6. express.urlencoded - allows us to read any "HTML form data" body of a request
 app.use(express.urlencoded({ extended: true }))
 
-// 6. express.static - allows us to serve static files
+// 7. express.static - allows us to serve static files ( in this case from public folder )
 app.use(express.static('public'))
 
 
 // error handling middleware
+// any error that is thrown in the middlewares will be caught here
 app.use((err, req, res, next) => {
     console.log(err)
-    // TODO: log this error to file with morgan somehow
     accessLogStream.write(` ${req.method} ${req.path} ${err.message} \n`)
     res.status(500).send('Something broke!')
 })
 
+// END OF TOP TEN MIDDLEWARES
 app.get("/test", (req, res) => {
     console.log(req.sessionID)
     res.status(200).end()
@@ -66,15 +69,6 @@ app.get("/test", (req, res) => {
 // authentication???
 
 // authentication??? OKTA, 0auth, passport, etc.
-
-
-app.get("/data", (req, res) => {
-    res.json({hello:"world"})
-})
-app.post("/login", (req, res) => {
-//    User.find({user:req.body.username, password:req.body.password})
-   res.json("congrats your logged in")
-})
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080')
